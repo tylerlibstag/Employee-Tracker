a517ddd14e02f026f8b6e7ef56ff3fce69b86eb9
TYTY <tylerlibstag@gmail.com>
2020-08-02 22:13:09 -0400

    added files

---
+++ db/connection.js
@@ -0,0 +1,21 @@
+const util = require("util");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  // Your username
+  user: "root",
+  // Your password
+  password: "password",
+  database: "employees"
+});
+
+connection.connect(function(err) {
+ // console.log('err >', err)
+});
+
+// Setting up connection.query to use promises instead of callbacks
+// This allows us to use the async/await syntax
+//connection.query = util.promisify(connection.query);
+
+module.exports = connection;
